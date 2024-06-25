fb085a4edbcfeab1a71a145df7b8d50794d69177
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-24 21:01:38 -0300

    metodo GET listagem herois

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get("/", function(req, res){
-    res.send("PALMEIRAS MAIOR CAMPEÃO DO BRASIL")
+const heroes =["Homem-Aranha, Hulk, Ms marvel"]
+
+//ENDPOINT listagem de todos os heróis (READ ALL ==> [GET])
+app.get("/heroes", function(req,res){
+    res.send(heroes)
 })
 
-app.get("/ptbr", function(req,res){
-    res.send("Chupa Gambá")
+app.get("/heroes/:id", function(req,res){
+    //acessar o parametro da rota ID
+    const id = req.params.id
+    //pega o item do array
+    const umHeroi = heroes[id]
+    // envio o item encontrado
+    res.send(umHeroi)
 })
+
 app.listen(3000)
 
-app.post()
+/*app.post()
 app.put()
 app.patch()
 app.listen()
-app.delete()
+app.delete()*/
+
