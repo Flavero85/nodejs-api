eb37842e7a0c5706c18f9a9b6ba3bf438ef5a24e
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-24 21:11:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const app = express()
 
-const heroes =["Homem-Aranha, Hulk, Ms marvel"]
+const herois =["Homem-Aranha", "Hulk", "Ms marvel"]
 
 //ENDPOINT listagem de todos os heróis (READ ALL ==> [GET])
-app.get("/heroes", function(req,res){
-    res.send(heroes)
+app.get("/herois", function(req,res){
+    res.send(herois)
 })
 
-app.get("/heroes/:id", function(req,res){
+app.get("/herois/:id", function(req,res){
     //acessar o parametro da rota ID
     const id = req.params.id
     //pega o item do array
-    const umHeroi = heroes[id]
+    const umHeroi = herois[id]
     // envio o item encontrado
     res.send(umHeroi)
+})
+
+app.post("/herois", function(req,res){
+    res.send(req.body)
 })
 
 app.listen(3000)
